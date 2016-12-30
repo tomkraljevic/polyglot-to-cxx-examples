bfbe522de19465f993f7152ad816a3f60a506880
Tom Kraljevic <tomk@tomk.net>
2016-12-30 13:28:40 -0800

    Fix PEP8 whitespace issues.

--- python/setup.py
+++ python/setup.py
@@ -1,20 +1,19 @@
 from distutils.core import setup, Extension
-import os
 
 module1 = Extension('_foolib',
-                    define_macros = [('MAJOR_VERSION', '1'),
-                                     ('MINOR_VERSION', '0')],
-                    include_dirs = ['cxx/include'],
-                    sources = ['foolib/foolibmodule.c', os.path.join('cxx/src/foolib_c.cxx')])
+                    define_macros=[('MAJOR_VERSION', '1'),
+                                   ('MINOR_VERSION', '0')],
+                    include_dirs=['cxx/include'],
+                    sources=['foolib/foolibmodule.c', 'cxx/src/foolib_c.cxx'])
 
-setup (name = 'foolib',
-       version = '1.0',
-       description = 'This is a demo package',
-       author = 'Tom Kraljevic',
-       author_email = 'tomk@tomk.net',
-       url = 'http://example-of-where-to-put-url.org',
-       long_description = '''
+setup(name='foolib',
+      version='1.0',
+      description='This is a demo package',
+      author='Tom Kraljevic',
+      author_email='tomk@tomk.net',
+      url='http://example-of-where-to-put-url.org',
+      long_description='''
 This is really just a demo package.
 ''',
-       packages = ['foolib'],
-       ext_modules = [module1])
+      packages=['foolib'],
+      ext_modules=[module1])
