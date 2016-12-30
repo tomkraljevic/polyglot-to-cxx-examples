e75364fad57121ac7a4567ab2b24819d90385adb
Tom Kraljevic <tomk@tomk.net>
2016-12-30 13:53:13 -0800

    Another iteration on the easy api example.

--- python/foolib/tests/test_easy.py
+++ python/foolib/tests/test_easy.py
@@ -1,23 +1,39 @@
 import h2omojo
 
 
+# To avoid bouncing between Python and C++ languages, the load_mojo_model() returns an Easy API object which
+# additionally exposes the raw MojoModel methods like score0().
+#
+# C++ returns an Easy API object instance which wraps a raw instance, but from Python's perspective, there
+# is only one object to deal with.
+#
+# The Python wrapper has a __del__ method which naturally releases the underlying C++ object when the
+# refcount drops to 0.
+
+model = h2omojo.load_mojo_model(mojo_path="/path/to/mojo.zip",
+                                convert_unknown_categorical_levels_to_na=False)
+
+
 #
 # Raw API score0() example
 #
 
-rawModel = h2omojo.load_mojo_model("path/to/mojo")
-
 row2 = []
-n = rawModel.nfeatures()
+n = model.nfeatures()
 for i in range(n):
     row2.append(0.0)
+row2[0] = 68
+row2[1] = 2
+row2[2] = 2
+row2[3] = 0
+row2[4] = 6
 
 pred2 = []
-n = rawModel.getPredsSize()
+n = model.getPredsSize()
 for i in range(n):
     pred2.append(0.0)
 
-rawModel.score0(row2, pred2)
+model.score0(row2, pred2)
 
 for (i in range(len(pred2))):
     print(pred2[i]);
@@ -26,15 +42,6 @@ for (i in range(len(pred2))):
 #
 # Easy API example
 #
-
-# To avoid bouncing between Python and C++ languages, the load_mojo_model() returns an Easy API object which
-# also exposes the raw MojoModel methods like score0().  This also avoids tricky reference issues.
-# C++ returns an Easy API object instance which wraps a raw instance, but from Python's perspective, there
-# is only one object to deal with.
-#
-# The Python wrapper has a __del__ method which naturally releases the underlying C++ object when the
-# refcount drops to 0.
-model = rawModel
 
 # Pure dict row case
 
@@ -45,20 +52,11 @@ row['DCAPS'] = '2'
 row['VOL'] = '0'
 row['GLEASON'] = '6'
 
-# RowData object case
-
-# row = h2omojo.RowData()
-# row.put("AGE", "68")
-# row.put("RACE", "2")
-# row.put("DCAPS", "2")
-# row.put("VOL", "0")
-# row.put("GLEASON", "6")
-
 p = model.predict_binomial(row)
 
-print("Has penetrated the prostatic capsule (1=yes; 0=no): " + p.get_label())
+print("Has penetrated the prostatic capsule (1=yes; 0=no): " + p.label)
 print("Class probabilities: ")
-for i in range(len(p.class_probabilities())):
+for i in range(len(p.class_probabilities)):
     if i > 0:
         print(",")
     print(p.class_probabilities[i])
